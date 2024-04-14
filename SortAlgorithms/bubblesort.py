f69201d50f817406130f05266af8034efae524e7
quangliz <quwork.12@gmail.com>
2024-04-14 09:12:50 +0700

    add bubble sort

---
+++ SortAlgorithms/bubblesort.py
@@ -0,0 +1,16 @@
+import random
+def bubbleSort(array):
+    indexOfLastUnsortedElement = len(array)
+    for i in range(len(array)):
+        swap = False
+        for j in range(indexOfLastUnsortedElement - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                swap = True
+        if swap == False: break
+        indexOfLastUnsortedElement -= 1
+arr = [random.randint(0, 100) for _ in range(20)]
+print(arr)
+bubbleSort(arr)
+for i in range(len(arr)):
+    print(arr[i], end = " ")
